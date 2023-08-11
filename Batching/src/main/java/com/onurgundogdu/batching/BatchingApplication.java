26c0abb59d9ce09c74c08d6bdefc46c91634781a
Onur Gündoğdu <139327017+onurgndgdu@users.noreply.github.com>
2023-08-11 18:07:05 +0300

    created pom.xml and application.properties

---
+++ Batching/src/main/java/com/onurgundogdu/batching/BatchingApplication.java
@@ -0,0 +1,13 @@
+package com.onurgundogdu.batching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchingApplication.class, args);
+    }
+
+}
