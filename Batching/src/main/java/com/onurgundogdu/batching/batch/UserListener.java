41a6bc7786a898a353391eac1a12ec48626fb22c
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-12 19:24:08 +0300

    used jobExecution and added class

--- Batching/src/main/java/com/onurgundogdu/batching/batch/UserListener.java
+++ Batching/src/main/java/com/onurgundogdu/batching/batch/UserListener.java
@@ -1,7 +1,16 @@
 package com.onurgundogdu.batching.batch;
 
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 
 public class UserListener implements JobExecutionListener {
 
+    public void beforeJob(JobExecution jobExecution){
+        System.out.println("Job Started : "+jobExecution.getStartTime());
+        System.out.println("Job Status : "+jobExecution.getStatus());
+    }
+    public void afterJob(JobExecution jobExecution){
+        System.out.println("Job Ended : "+jobExecution.getEndTime());
+        System.out.println("Job Status : "+jobExecution.getStatus());
+    }
 }
