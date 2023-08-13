cd7affcba81aeed045723a873e4f05a40d426c2d
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-13 16:05:08 +0300

    added ItemProcessor class

---
+++ Batching/src/main/java/com/onurgundogdu/batching/input/CustomerInput.java
@@ -0,0 +1,59 @@
+package com.onurgundogdu.batching.input;
+
+public class CustomerInput {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String phoneNumber;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
