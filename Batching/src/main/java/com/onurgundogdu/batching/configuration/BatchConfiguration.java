0a73552bc041a4d3609b86a53af7332f7681e83a
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-19 15:02:23 +0300

    added reader and writer  batch conf class

--- Batching/src/main/java/com/onurgundogdu/batching/configuration/BatchConfiguration.java
+++ Batching/src/main/java/com/onurgundogdu/batching/configuration/BatchConfiguration.java
@@ -1,6 +1,6 @@
 package com.onurgundogdu.batching.configuration;
 
-import com.onurgundogdu.batching.input.CustomerInput;
+import com.onurgundogdu.batching.item.CustomerItemProcessor;
 import com.onurgundogdu.batching.listener.JobCompletionNotificationListener;
 import com.onurgundogdu.batching.model.Customer;
 import org.springframework.batch.core.Job;
@@ -10,14 +10,49 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names(new String[]{"firstName", "lastName"})
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<Customer>() {{
+                    setTargetType(Customer.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public CustomerItemProcessor processor() {
+        return new CustomerItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Customer> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Customer>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO customer (first_name, last_name, age, email, phone_number ) VALUES (:firstName, :lastName, :age, :email, :phoneNumber)")
+                .dataSource(dataSource)
+                .build();
+    }
     @Bean
     public Job importUserJob(JobRepository jobRepository,
                              JobCompletionNotificationListener listener, Step step1) {
@@ -39,4 +74,5 @@ public class BatchConfiguration {
                 .writer(writer)
                 .build();
     }
+
 }
