950de004f8c66a37183da013092866d94deda4e1
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-19 14:55:41 +0300

    added JobBuilder in Configuration

---
+++ Batching/src/main/java/com/onurgundogdu/batching/configuration/BatchConfiguration.java
@@ -0,0 +1,42 @@
+package com.onurgundogdu.batching.configuration;
+
+import com.onurgundogdu.batching.input.CustomerInput;
+import com.onurgundogdu.batching.listener.JobCompletionNotificationListener;
+import com.onurgundogdu.batching.model.Customer;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+    @Bean
+    public Job importUserJob(JobRepository jobRepository,
+                             JobCompletionNotificationListener listener, Step step1) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Customer> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<Customer, Customer> chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
+                .build();
+    }
+}
