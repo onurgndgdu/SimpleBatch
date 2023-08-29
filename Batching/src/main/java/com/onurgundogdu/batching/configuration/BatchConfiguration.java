10ddd43a0ccbdaa091bcb4028288cf15ccfec96e
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-29 23:27:05 +0300

    added scheduled

--- Batching/src/main/java/com/onurgundogdu/batching/configuration/BatchConfiguration.java
+++ Batching/src/main/java/com/onurgundogdu/batching/configuration/BatchConfiguration.java
@@ -3,32 +3,43 @@ package com.onurgundogdu.batching.configuration;
 import com.onurgundogdu.batching.item.CustomerItemProcessor;
 import com.onurgundogdu.batching.listener.JobCompletionNotificationListener;
 import com.onurgundogdu.batching.model.Customer;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.transaction.PlatformTransactionManager;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(BatchConfiguration.class);
+
+
+    private final JobLauncher jobLauncher;
+    private final Job importUserJob;
+
+    public BatchConfiguration(JobLauncher jobLauncher, Job importUserJob) {
+        this.jobLauncher = jobLauncher;
+        this.importUserJob = importUserJob;
+    }
     @Bean
     public FlatFileItemReader<Customer> reader() {
         return new FlatFileItemReaderBuilder<Customer>()
@@ -80,5 +91,19 @@ public class BatchConfiguration {
                 .flow(step1)
                 .end()
                 .build();
+    }
+
+    @Scheduled(cron = "0 0 * * *") // Her saat başında çalışır
+    public void runBatchJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+
+        try {
+            JobExecution jobExecution = jobLauncher.run(importUserJob, jobParameters);
+            log.info("Batch job finished with status: " + jobExecution.getStatus());
+        } catch (Exception e) {
+            log.info("Error executing batch job: " + e.getMessage());
+        }
     }
 }
--- Batching/src/main/java/com/onurgundogdu/batching/listener/JobCompletionNotificationListener.java
+++ Batching/src/main/java/com/onurgundogdu/batching/listener/JobCompletionNotificationListener.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +17,14 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
     private final JdbcTemplate jdbcTemplate;
 
+
     public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        // Before job execution logic, if needed
+        // Job başlamadan önce yapılacak işlemler
     }
 
     @Override
@@ -32,11 +34,11 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
             jdbcTemplate.query("SELECT first_name, last_name, age, email, phone_number FROM customer",
                     (rs, row) -> new Customer(
-                            rs.getString("first_name"),
-                            rs.getString("last_name"),
-                            rs.getInt("age"),
-                            rs.getString("email"),
-                            rs.getString("phone_number"))
+                            rs.getString(1),
+                            rs.getString(2),
+                            rs.getInt(3),
+                            rs.getString(4),
+                            rs.getString(5))
             ).forEach(customer -> log.info("Found {} in the database.", customer));
         }
     }
